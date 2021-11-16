b26559639a3eeee9c903ad6564a68cc580003ec2
illinois <thisPC@comp.com>
2021-11-16 14:57:39 +0700

    work with ytdlcore

--- VideoService.js
+++ VideoService.js
@@ -1,6 +1,7 @@
 const HTTPS = require('https');
 const FS = require('fs');
 const FormData = require('form-data');
+const ytdl = require('ytdl-core');
 const FFMPEG = require('fluent-ffmpeg');
 FFMPEG.setFfmpegPath(require('@ffmpeg-installer/ffmpeg').path);
 const Express = require('express');
@@ -59,9 +60,10 @@ var CheckStatus = hash => new Promise((resolve, reject) => {
     .on('error', err => reject(err));
 })
 
-server.post('/', function(Request, Response) {
-    var Data = Request.body, FileID, FileMP4;
+server.get('/', (Request, Response) => Response.send('App is working'))
 
+server.post('/yarncore', function(Request, Response) {
+    var Data = Request.body, FileID, FileMP4;
     if(Data.URL.toLowerCase().includes('y.yarn.co'))
     {
         FileID = Data.URL.split('/').pop();
@@ -70,8 +72,8 @@ server.post('/', function(Request, Response) {
         HTTPS.get(Data.URL, function(file) {
             file.pipe(FileMP4);
         })
-            .on("error", () => {
-                Response.json({ message: "Couldn't download the video" });
+        .on("error", () => {
+            Response.json({ message: "Couldn't download the video" });
         })
         .on("close", async () => {
             // send to server
@@ -101,65 +103,15 @@ server.post('/', function(Request, Response) {
             }
         })
     }
-    // else if(Data.URL.toLowerCase().includes('youtu.be'))
-    // {
-        // let T1 = Number(Data.Time1.split(':')[0]) * 3600 + Number(Data.Time1.split(':')[1]) * 60 + Number(Data.Time1.split(':')[2]);
-        // let T2 = Number(Data.Time2.split(':')[0]) * 3600 + Number(Data.Time2.split(':')[1]) * 60 + Number(Data.Time2.split(':')[2]);
-        // let Duration = T2 - T1;
-        // FileID = 'input';// ??
-        // FileMP4 = FS.createWriteStream(`./downloads/${FileID}.mp4`);
-        // HTTPS.get(`https://y.yarn.co/${FileID}.mp4`, function(file) {
-        //     file.pipe(FileMP4);
-        // })
-        // .on("error", () => {
-        //     Response.send("Не удалось скачать видео");
-        // })
-        // .on("close", () => {
+    else Response.status(400).json({ message: "Couldn't process the url" });
+})
 
-/****************************************************************** */
-            // FFMPEG(`./downloads/${FileID}.mp4`)
-            // .setStartTime(Data.Time1)
-            // .setDuration(Duration)
-            // .output(`./downloads/${FileID}_cropped.mp4`)
-            // .on("error", err => {
-            //     console.log(err);
-            //     Response.send("Не удалось обработать видео");
-            // })
-            // .on("end", async () => {
-            //     // send to server
-            //     try{
-            //         let result = await Upload(FileID+'_cropped');
-            //         // FS.unlink(`./downloads/${FileID}.mp4`, (err) => { 
-            //         //     if (err)    console.log(err);
-            //         // })
-            //         FS.unlink(`./downloads/${FileID}_cropped.mp4`, (err) => { 
-            //             if (err)    console.log(err);
-            //         })
+server.post('/ytcore', function(Request, Response) {
+    var Data = Request.body, FileID, FileMP4;
+    if(Data.URL.toLowerCase().includes('youtube'))
+    {
 
-            //         // wait for finish
-            //         let status;
-            //         do {
-            //             await sleep(delay);
-            //             status = await CheckStatus(result.hash);
-            //         } while(status != 'finished')
-
-            //         Response.json({
-            //             info: 'Uploaded!',
-            //             hash: result.hash,
-            //             status: status,
-            //             url: result.url
-            //         })
-            //     }
-            //     catch(ex){
-            //         console.log(ex);
-            //         Response.send("something went wrong");
-            //     }
-            // }).run();
-/****************************************************************** */
-
-        // })
-    // }
-    else Response.json({ message: "Couldn't process the url" });
+    }
 })
 
 server.listen(Port, ()=>{
@@ -167,3 +119,62 @@ server.listen(Port, ()=>{
     if (!FS.existsSync(dir))    FS.mkdirSync(dir);
     console.log('Server run on port: '+Port);
 });
+
+// else if(Data.URL.toLowerCase().includes('youtu.be'))
+// {
+    // let T1 = Number(Data.Time1.split(':')[0]) * 3600 + Number(Data.Time1.split(':')[1]) * 60 + Number(Data.Time1.split(':')[2]);
+    // let T2 = Number(Data.Time2.split(':')[0]) * 3600 + Number(Data.Time2.split(':')[1]) * 60 + Number(Data.Time2.split(':')[2]);
+    // let Duration = T2 - T1;
+    // FileID = 'input';// ??
+    // FileMP4 = FS.createWriteStream(`./downloads/${FileID}.mp4`);
+    // HTTPS.get(`https://y.yarn.co/${FileID}.mp4`, function(file) {
+    //     file.pipe(FileMP4);
+    // })
+    // .on("error", () => {
+    //     Response.send("Не удалось скачать видео");
+    // })
+    // .on("close", () => {
+
+/****************************************************************** */
+        // FFMPEG(`./downloads/${FileID}.mp4`)
+        // .setStartTime(Data.Time1)
+        // .setDuration(Duration)
+        // .output(`./downloads/${FileID}_cropped.mp4`)
+        // .on("error", err => {
+        //     console.log(err);
+        //     Response.send("Не удалось обработать видео");
+        // })
+        // .on("end", async () => {
+        //     // send to server
+        //     try{
+        //         let result = await Upload(FileID+'_cropped');
+        //         // FS.unlink(`./downloads/${FileID}.mp4`, (err) => { 
+        //         //     if (err)    console.log(err);
+        //         // })
+        //         FS.unlink(`./downloads/${FileID}_cropped.mp4`, (err) => { 
+        //             if (err)    console.log(err);
+        //         })
+
+        //         // wait for finish
+        //         let status;
+        //         do {
+        //             await sleep(delay);
+        //             status = await CheckStatus(result.hash);
+        //         } while(status != 'finished')
+
+        //         Response.json({
+        //             info: 'Uploaded!',
+        //             hash: result.hash,
+        //             status: status,
+        //             url: result.url
+        //         })
+        //     }
+        //     catch(ex){
+        //         console.log(ex);
+        //         Response.send("something went wrong");
+        //     }
+        // }).run();
+/****************************************************************** */
+
+    // })
+// }
---
+++ ytdl.js
@@ -0,0 +1,37 @@
+const FS = require('fs');
+const ytdl = require('ytdl-core');
+const HTTPS = require('https');
+
+async function videoformat(ytburl){
+    const _videoid = ytdl.getURLVideoID(ytburl);
+    const info = await ytdl.getInfo(ytburl);
+    try{
+        const format = ytdl.chooseFormat(info.formats, { quality: '22' }); //720p
+        return { link: format.url, videoid: _videoid, container: format.container };
+    }
+    catch(ex) { }
+    try{
+        const format = ytdl.chooseFormat(info.formats, { quality: '18' }); //360p or possible
+        return { link: format.url, videoid: _videoid, container: format.container };
+    }
+    catch(ex) { }
+}
+
+// 18 - 360p, 22 - 720p, 
+// with 1080p
+const url1 = 'https://www.youtube.com/watch?v=KwXKTUOydKQ';
+const url2 = 'https://www.youtube.com/watch?v=rq8Aeq2RIf4';
+if(!ytdl.validateURL(url1)){
+    return;
+}
+
+const video = await videoformat(url1);
+console.log(video);
+// HTTPS.get(video.link, file => {
+//     file.pipe(FS.createWriteStream(video.videoid+'.'+video.container));
+// });
+//or
+// ytdl('https://www.youtube.com/watch?v=KwXKTUOydKQ', { quality: 22 }).pipe(FS.createWriteStream(video.videoid+'.mp4'));
+
+//with 360p
+// ytdl('https://www.youtube.com/watch?v=rq8Aeq2RIf4', { quality: 18 }).pipe(fs.createWriteStream('video.mp4'));
