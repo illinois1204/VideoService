2cb1cc8430597447efb587e9436384ca4992236b
illinois <boowoa@beaa.com>
2021-11-17 05:42:50 +0700

    вернул async

--- ytdl.js
+++ ytdl.js
@@ -17,21 +17,25 @@ async function videoformat(ytburl){
     catch(ex) { }
 }
 
-// 18 - 360p, 22 - 720p, 
-// with 1080p
-const url1 = 'https://www.youtube.com/watch?v=KwXKTUOydKQ';
-const url2 = 'https://www.youtube.com/watch?v=rq8Aeq2RIf4';
-if(!ytdl.validateURL(url1)){
-    return;
+async function main(){
+    // 18 - 360p, 22 - 720p, 
+    // with 1080p
+    const url1 = 'https://www.youtube.com/watch?v=KwXKTUOydKQ';
+    const url2 = 'https://www.youtube.com/watch?v=rq8Aeq2RIf4';
+    if(!ytdl.validateURL(url1)){
+        return;
+    }
+    
+    const video = await videoformat(url1);
+    console.log(video);
+    // HTTPS.get(video.link, file => {
+    //     file.pipe(FS.createWriteStream(video.videoid+'.'+video.container));
+    // });
+    //or
+    ytdl('https://www.youtube.com/watch?v=KwXKTUOydKQ', { quality: 22 }).pipe(FS.createWriteStream(video.videoid+'.mp4'));
+    
+    //with 360p
+    // ytdl('https://www.youtube.com/watch?v=rq8Aeq2RIf4', { quality: 18 }).pipe(fs.createWriteStream('video.mp4'));
 }
 
-const video = await videoformat(url1);
-console.log(video);
-// HTTPS.get(video.link, file => {
-//     file.pipe(FS.createWriteStream(video.videoid+'.'+video.container));
-// });
-//or
-// ytdl('https://www.youtube.com/watch?v=KwXKTUOydKQ', { quality: 22 }).pipe(FS.createWriteStream(video.videoid+'.mp4'));
-
-//with 360p
-// ytdl('https://www.youtube.com/watch?v=rq8Aeq2RIf4', { quality: 18 }).pipe(fs.createWriteStream('video.mp4'));
+main();
